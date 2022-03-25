18a41b53cb7ee5a37e5c5cc152aca1a0471eaa43
AlanCbunsan <alan.correa@bunsan.io>
2022-03-25 15:09:48 -0600

    new

---
+++ becarios_bunsan_2022_b1/test/DataDrivenTest.exs
@@ -0,0 +1,20 @@
+defmodule DataDrivenTest do
+use ExUnit.Case
+data = [
+    {1, 3, 4}
+    {7, 4, 10},
+    {5, 4, 9},
+    {3, 3, 6},
+    {6, 2, 9}
+] 
+for {a,b,c} <- data do
+ @a a
+ @b b
+ @c c
+ test  "sum of #{@a} and #{@b} should equal #{@c}" do
+    assert SUT.sum(@a,@b) == @c
+     
+ end     
+ end
+        
+end
